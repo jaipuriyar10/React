8f1d1469ce8ab24b3cea47c72beb16a919d34f7c
Divyansh Jaipuriyar <jaipuriyardivyansh10@gmail.com>
2022-05-22 18:32:37 +0530

    created class component with state updates

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-class App extends React.Component{
-    constructor(props)
-    {
-        super(props)
-        this.state = {lat:40};
-    }
-    render()
-    {
-        return <div>Latitude:{this.state.lat}</div>
-    }
+class App extends React.Component {
+
+    //initialise constructor at the first and call super() method with props following the compulsion of react
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+    window.navigator.geolocation.getCurrentPosition(
+      (pos) => {
+          //use setState for updating the state 
+        this.setState({ lat: pos.coords.latitude });
+      },
+      (err) => {
+        console.log(`---dj error message:`, err);
+      }
+    );
+  }
+  //render as per the compulsion of react
+  render() {
+    return <div>Latitude:{this.state.lat}</div>;
+  }
 }
 
-ReactDOM.render(<App/>,document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector("#root"));
