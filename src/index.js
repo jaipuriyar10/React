c4123f5249ef750787c37fa65210824777e97011
Divyansh Jaipuriyar <jaipuriyardivyansh10@gmail.com>
2022-05-22 19:08:44 +0530

    handling errorMessage properly

--- src/index.js
+++ src/index.js
@@ -6,20 +6,25 @@ class App extends React.Component {
     //initialise constructor at the first and call super() method with props following the compulsion of react
   constructor(props) {
     super(props);
-    this.state = { lat: null };
+    this.state = { lat: null,errorMessage:null };
     window.navigator.geolocation.getCurrentPosition(
       (pos) => {
           //use setState for updating the state 
         this.setState({ lat: pos.coords.latitude });
       },
       (err) => {
-        console.log(`---dj error message:`, err);
+          this.setState({errorMessage:err.message});
       }
     );
   }
   //render as per the compulsion of react
   render() {
-    return <div>Latitude:{this.state.lat}</div>;
+    return (<div>
+        Latitude:{this.state.lat}
+    <br/>
+        Error: {this.state.errorMessage}
+        </div>    
+    );
   }
 }
 
