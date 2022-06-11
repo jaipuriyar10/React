9e5453dcae84e629d1b79ecffef2ca068f889196
Divyansh Jaipuriyar <jaipuriyardivyansh10@gmail.com>
2022-06-11 18:13:11 +0530

    added semantic ui images for season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+// import 'semantic-ui-css/semantic.min.css'
 const Season = (lat,month)=>{
     //using ternary operator 
     return (lat>0)?(month>2 && month<9)?'Summer':'Winter':(month>2 && month<9)?'Winter':'Summer';
@@ -12,9 +12,12 @@ const SeasonDisplay = (props) =>{
     //details of props as it is an object
     const season = Season(props.lat,new Date().getMonth());
     const text= (season==='Summer')?`Let's go to Beach`:`It's chilly out here`;
+    const Icon = (season!==`Summer`)?'snowflake':'sun';
     console.log(`--details of props:`,props);
-    return (
+    return (<div>
+        <i className={`${Icon} icon`}></i>
         <h1>{text}</h1>
+        </div>
     );
 };
 
